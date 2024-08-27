1c4344d8b439352bc245a2012e27dfd7d42ec0de
Kiril Isakov <kirix.isakov@gmail.com>
2024-08-27 16:22:59 +0200

    refactor in order to DL from multiple URLs in a row: - use `text_area` instead of `text_input`; - show multiple stacked `toast` bubbles instead of a single empty placeholder; - a sample of short videos for testing purposes (`test_urls.txt`); - follow recommended best practices:     - use constants instead of global vars     - move all functions above the main part     - encapsulate main part in `if __name__ == '__main__':`     - avoid code duplication:         - merge similar functions         - use shorter functions and wrappers

--- dashboard_main.py
+++ dashboard_main.py
@@ -1,76 +1,87 @@
-import os
-import pandas as pd
-from main import download_video, download_video_audio, save_downloaded_files
-import streamlit as st 
-
-st.set_page_config(layout='wide', page_title='Youtube Video Downloader', page_icon="logo.png")
-
-column1, column2 = st.columns([1,15])
-
-with column1:
-    st.image("logo.png", width=80)
-with column2:
-    st.title("Youtube Video Downloader")
-
-st.divider()
-user_input = st.text_input("Youtube URL data", key="url")
-st.divider()
-column1, column2 = st.columns([1, 6])
-
-message_placeholder = st.empty()
-
-def handle_downloaded_video():
-    if user_input:
-        with st.spinner("Downloading video..."):
-            result, file_path = download_video(user_input)
-            message_placeholder.success(result)
-            save_downloaded_files()
-            return file_path
-
-def handle_downloaded_audio():
-    if user_input:
-        with st.spinner("Downloading audio..."):
-            result, file_path = download_video_audio(user_input)
-            message_placeholder.success(result)
-            return file_path
-
-video_path = None
-audio_path = None
-
-with column1:
-    if st.button("I want download a Video", help="Download your Youtube Video", use_container_width=False):
-        video_path = handle_downloaded_video()
-
-with column2:
-    if st.button("I want download only the Audio", help="Download audio from your Youtube Video", use_container_width=False):
-        audio_path = handle_downloaded_audio()
-
-if video_path and os.path.exists(video_path):
-    with open(video_path, 'rb') as file:
-        download_button = st.download_button(
-            label="Download Video",
-            data=file.read(),
-            file_name=os.path.basename(video_path),
-            mime="video/mp4",
-            use_container_width=True
-        )
-        if download_button:
-            os.remove(audio_path)
-            st.write(f"File {os.path.basename(video_path)} has been deleted.")
-
-if audio_path and os.path.exists(audio_path):
-    with open(audio_path, 'rb') as file:
-        download_button = st.download_button(
-            label="Download Audio",
-            data=file.read(),
-            file_name=os.path.basename(audio_path),
-            mime="audio/mpeg",
-            use_container_width=True
-        )    
-        if download_button:
-            os.remove(audio_path)
-            st.write(f"File {os.path.basename(audio_path)} has been deleted.")
+from main import download_video, download_video_audio, log_downloaded_files
+import streamlit as st
 
 
+def download_all_urls_and_log(user_input, *, file_extension):
+    def is_mp3():
+        return file_extension.casefold() == 'mp3'.casefold()
+
+    if not user_input:
+        return
+    callback_func = download_video_audio if is_mp3() else download_video
+    content_type = 'Audio' if is_mp3() else 'Video'
+    for url in user_input.split('\n'):
+        if url.strip() == '':
+            continue
+        message_placeholder = st.toast(f'Downloading {content_type.lower()}...')
+        result_msg, title, file_path = callback_func(url.strip())
+        if file_path is None:
+            message_placeholder.toast(result_msg, icon='❌')
+            continue
+        else:
+            msg_with_data = result_msg.format(title, file_path)
+            message_placeholder.toast(msg_with_data, icon='✅')
+            log_downloaded_files(content_type, title, file_path)
 
 
+def download_all_urls_as_video():
+    download_all_urls_and_log(user_input, file_extension='mp4')
+
+
+def download_all_urls_as_audio():
+    download_all_urls_and_log(user_input, file_extension='mp3')
+
+
+if __name__ == '__main__':
+    st.set_page_config(
+        layout='wide', page_title='Youtube Video Downloader', page_icon='logo.png')
+
+    column1, column2 = st.columns([1, 15])
+
+    with column1:
+        st.image('logo.png', width=80)
+    with column2:
+        st.title('Youtube Video Downloader')
+
+    st.divider()
+    user_input = st.text_area(
+        'Paste one or more YouTube URLs, one per line:', key='url')
+    st.divider()
+    column1, column2 = st.columns([1, 6])
+
+    with column1:
+        st.button('I want download a Video', help='Download your Youtube Video',
+                  on_click=download_all_urls_as_video, use_container_width=False)
+
+    with column2:
+        st.button('I want download only the Audio', help='Download audio from your Youtube Video',
+                  on_click=download_all_urls_as_audio, use_container_width=False)
+
+# video_path = None
+# audio_path = None
+
+# if video_path and os.path.exists(video_path):
+#     with open(video_path, 'rb') as file:
+#         download_button = st.download_button(
+#             label='Download Video',
+#             data=file.read(),
+#             file_name=os.path.basename(video_path),
+#             mime='video/mp4',
+#             use_container_width=True
+#         )
+#         if download_button:
+#             os.remove(audio_path)
+#             st.write(f'File {os.path.basename(video_path)} has been deleted.')
+
+# if audio_path and os.path.exists(audio_path):
+#     with open(audio_path, 'rb') as file:
+#         download_button = st.download_button(
+#             label='Download Audio',
+#             data=file.read(),
+#             file_name=os.path.basename(audio_path),
+#             mime='audio/mpeg',
+#             use_container_width=True
+#         )
+#         if download_button:
+#             os.remove(audio_path)
+#             st.write(f'File {os.path.basename(audio_path)} has been deleted.')
--- main.py
+++ main.py
@@ -1,50 +1,203 @@
 import os
 import pandas as pd
+
+
+
 from pytubefix import YouTube
+
+
+
 from pytubefix.cli import on_progress
- 
-download_video_folder = 'download_videos'
-download_audio_folder = 'download_audios'
 
-if not os.path.exists(download_video_folder):
-     os.makedirs(download_video_folder)
-    
-if not os.path.exists(download_audio_folder):
-     os.makedirs(download_audio_folder)
 
-downloaded_files = pd.DataFrame(columns=['Type', 'Title', 'Path'])
+
+
+CSV_FILE_NAME = 'downloaded_files.csv'
+
+
+
+AUD_CONTENT_DIR = 'downloaded_audios'
+
+
+
+VID_CONTENT_DIR = 'downloaded_videos'
+
+
+
+RESULT_MSG_AUDIO = 'Your music “{0}” has been downloaded to {1}'
+
+
+
+RESULT_MSG_VIDEO = 'Your video “{0}” has been downloaded to {1}'
+
+
+
+ERR_MSG_BAD_URL = 'This URL is not recognized. Please insert a URL from a YouTube video.'
+
+
+
+ERR_MSG_PRIVATE = 'This video is set as private by its owner. Please select another video.'
+
+
+
+
+
+def download_content(url: str, file_extension: str) -> (str, str, str):
+
+
+
+    def is_mp3():
+
+
+
+        return file_extension.casefold() == 'mp3'.casefold()
+
+
+
+
+    destination_dir = AUD_CONTENT_DIR if is_mp3() else VID_CONTENT_DIR
+
+
+
+    result_msg = RESULT_MSG_AUDIO if is_mp3() else RESULT_MSG_VIDEO
+
+
+
+    if not os.path.exists(destination_dir):
+
+
+
+        os.makedirs(destination_dir)
+
+
+
+    file_path = None
+
+
+
+    try:
+
+
+
+        yt = YouTube(url, on_progress_callback=on_progress)
+
+
+
+        if is_mp3():
+
+
+
+            ys = yt.streams.get_audio_only()
+
+
+
+        else:
+
+
+
+            ys = yt.streams.get_highest_resolution()
+
+
+
+        file_path = ys.download(
+
+
+
+            output_path=destination_dir,
+
+
+
+            filename=f'{yt.title}.{file_extension.lower()}')
+
+
+
+
+    except Exception as e:
+
+
+
+        if 'mregex_search' in str(e):
+
+
+
+            result_msg = ERR_MSG_BAD_URL
+
+
+
+        result_msg = ERR_MSG_PRIVATE
+
+
+
+    return result_msg, yt.title, file_path
+
+
+
 
 
 def download_video(url):
-    file = ""
-    try:
-        yt = YouTube(url, on_progress_callback = on_progress)        
-        ys = yt.streams.get_highest_resolution()
-        file = ys.download(output_path=download_video_folder)
-        file_path = os.path.join(download_video_folder, f"{yt.title}.mp4")
-        downloaded_files.loc[len(downloaded_files)]=['Video', yt.title, file_path]
-        save_downloaded_files()
-        return f"Your video is ready to download.", file
-    except Exception as e:
-        if "mregex_search" in str(e):
-            return f"This url is not recognized. Please insert a url from a Youtube video.", file
-        return f"This video is set as private by its owner. Please select another video.", file
+
+
+
+    return download_content(url, file_extension='mp4')
+
+
+
+
 
 def download_video_audio(url):
-    file = ""
-    try:
-        yt = YouTube(url, on_progress_callback = on_progress)
-        ys = yt.streams.get_audio_only()
-        file = ys.download(output_path=download_audio_folder, filename=f"{yt.title}.mp3")
-        file_path = os.path.join(download_audio_folder, f"{yt.title}.mp3")
-        downloaded_files.loc[len(downloaded_files)]=['Audio', yt.title, file_path]
-        save_downloaded_files()
-        return f"Your music is ready do download.", file
-    except Exception as e:
-        if "mregex_search" in str(e):
-            return f"This url is not recognized. Please insert a url from a Youtube video.", file
-        return f"This video is set as private by its owner. Please select another video.", file
-    
 
-def save_downloaded_files():
-     downloaded_files.to_csv('downloaded_files.csv', index=False)
+
+
+    return download_content(url, file_extension='mp3')
+
+
+
+
+def log_downloaded_files(content_type, title, path):
+
+
+
+    downloaded_files = pd.DataFrame(columns=['Type', 'Title', 'Path'])
+
+
+
+    if os.path.exists(CSV_FILE_NAME):
+
+
+
+        downloaded_files = pd.read_csv(CSV_FILE_NAME)
+
+
+
+    df_tmp = pd.DataFrame({'Type': content_type,
+
+
+
+                           'Title': title,
+
+
+
+                           'Path': os.path.abspath(path)
+
+
+
+                           }, index=[0])
+
+
+
+    downloaded_files = pd.concat([downloaded_files, df_tmp],
+
+
+
+                                 ignore_index=True,
+
+
+
+                                 sort=False)
+
+
+
+    downloaded_files.to_csv(CSV_FILE_NAME, index=False)
+
+
+
