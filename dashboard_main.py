bde8432a1b5b729ef3c2157326eef5677abd24fb
Kiril Isakov <kirix.isakov@gmail.com>
2024-08-29 19:26:26 +0200

    Merge branch "dev" into "main" Signed-off-by: Kiril Isakov <kirix.isakov@gmail.com>
    Squashed commit of the following:
    
    commit d6e1dfe9ef322b7149789e220a4c6c9c605a63df
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Thu Aug 29 17:14:38 2024 +0200
    
        fix: improve the app's responsive design
    
    commit 5a67eb11d9726f1cab286bceddc79bf585cbf52e
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Thu Aug 29 16:58:39 2024 +0200
    
        fix: make new buttons appear at the bottom of the page, not top
    
    commit ac21728fc6922dd6d23496254213871f05d33fd8
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Thu Aug 29 16:56:53 2024 +0200
    
        fix all buttons disappearing after a click:
        - wrap button in a fragment to prevent complete app rerunning after a click;
        - see documentation:
            - https://docs.streamlit.io/develop/api-reference/widgets/st.download_button
            - https://docs.streamlit.io/develop/concepts/architecture/fragments
    
    commit c379716a53afca477243890deabfe07909f25596
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Thu Aug 29 16:44:08 2024 +0200
    
        for each URL, insert a button to save media locally
    
    commit 0ef3d30938e960155ff0b5bbb75723e80d54bd30
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Thu Aug 29 16:28:25 2024 +0200
    
        `else` block is already implied
    
    commit 457a518cbca784938bc9c755f325d617b2ecf3d4
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Thu Aug 29 16:25:16 2024 +0200
    
        shorter info message for convenience in production
    
    commit 534b9d0260334efdc914004d12ba2d804a63ec05
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 20:24:17 2024 +0200
    
        remove empty lines, convert Win to Lin end-of-lines (CRLF to LF)
    
    commit 8b452b57ca0f3374659f3a6bcabc0409f0381d43
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 17:20:46 2024 +0200
    
        refactor in order to read URLs from any multiline string
    
    commit 1c4344d8b439352bc245a2012e27dfd7d42ec0de
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 16:22:59 2024 +0200
    
        refactor in order to DL from multiple URLs in a row:
        - use `text_area` instead of `text_input`;
        - show multiple stacked `toast` bubbles instead of a single empty placeholder;
        - a sample of short videos for testing purposes (`test_urls.txt`);
        - follow recommended best practices:
            - use constants instead of global vars
            - move all functions above the main part
            - encapsulate main part in `if __name__ == '__main__':`
            - avoid code duplication:
                - merge similar functions
                - use shorter functions and wrappers
    
    commit f717feaf262c0baac0bd187990bd1c94409e9776
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 15:27:55 2024 +0200
    
        update gitignore: untrack pycache & output directories
    
    commit e052a1d923e8705f4529df8d345be37ec150eb68
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 00:53:34 2024 +0200
    
        fix: set "mp3" as audio file ext by overriding the default ext "mp4"
    
    commit a02d0485042449423d95d6d73e183a12a1c778e0
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 00:50:44 2024 +0200
    
        use poetry as dependency manager
    
    commit 65e8e6ec20b14d91abbb22d2cd7585f6c1c25b3a
    Author: Kiril Isakov <kirix.isakov@gmail.com>
    Date:   Tue Aug 27 00:49:45 2024 +0200
    
        add .gitignore
    
    Signed-off-by: Kiril Isakov <kirix.isakov@gmail.com>

--- dashboard_main.py
+++ dashboard_main.py
@@ -1,76 +1,71 @@
 import os
-import pandas as pd
-from main import download_video, download_video_audio, save_downloaded_files
-import streamlit as st 
+from main import download_video, download_video_audio, log_downloaded_files
+import streamlit as st
 
-st.set_page_config(layout='wide', page_title='Youtube Video Downloader', page_icon="logo.png")
 
-column1, column2 = st.columns([1,15])
-
-with column1:
-    st.image("logo.png", width=80)
-with column2:
-    st.title("Youtube Video Downloader")
-
-st.divider()
-user_input = st.text_input("Youtube URL data", key="url")
-st.divider()
-column1, column2 = st.columns([1, 6])
-
-message_placeholder = st.empty()
-
-def handle_downloaded_video():
-    if user_input:
-        with st.spinner("Downloading video..."):
-            result, file_path = download_video(user_input)
-            message_placeholder.success(result)
-            save_downloaded_files()
-            return file_path
-
-def handle_downloaded_audio():
-    if user_input:
-        with st.spinner("Downloading audio..."):
-            result, file_path = download_video_audio(user_input)
-            message_placeholder.success(result)
-            return file_path
-
-video_path = None
-audio_path = None
-
-with column1:
-    if st.button("I want download a Video", help="Download your Youtube Video", use_container_width=False):
-        video_path = handle_downloaded_video()
-
-with column2:
-    if st.button("I want download only the Audio", help="Download audio from your Youtube Video", use_container_width=False):
-        audio_path = handle_downloaded_audio()
-
-if video_path and os.path.exists(video_path):
-    with open(video_path, 'rb') as file:
-        download_button = st.download_button(
-            label="Download Video",
-            data=file.read(),
-            file_name=os.path.basename(video_path),
-            mime="video/mp4",
+@st.fragment
+def insert_btn_to_save_media_locally(content_type, mime_type, title, file_path):
+    with open(file_path, 'rb') as file_bytes:
+        st.download_button(
+            label=f'Download “{title}” as {content_type}',
+            data=file_bytes.read(),
+            file_name=os.path.basename(file_path),
+            mime=f'{mime_type}',
             use_container_width=True
         )
-        if download_button:
-            os.remove(audio_path)
-            st.write(f"File {os.path.basename(video_path)} has been deleted.")
-
-if audio_path and os.path.exists(audio_path):
-    with open(audio_path, 'rb') as file:
-        download_button = st.download_button(
-            label="Download Audio",
-            data=file.read(),
-            file_name=os.path.basename(audio_path),
-            mime="audio/mpeg",
-            use_container_width=True
-        )    
-        if download_button:
-            os.remove(audio_path)
-            st.write(f"File {os.path.basename(audio_path)} has been deleted.")
 
 
+def download_all_urls_and_log(urls, *, file_extension):
+    def is_mp3():
+        return file_extension.casefold() == 'mp3'.casefold()
+
+    if not urls:
+        st.toast('No URL provided, so nothing to download', icon='⚠️')
+        return
+    callback_func = download_video_audio if is_mp3() else download_video
+    content_type = 'Audio' if is_mp3() else 'Video'
+    for url in urls:
+        msg_placeholder = st.toast(f'Downloading {content_type.lower()}...')
+        result_msg, title, file_path = callback_func(url.strip())
+        if file_path is None:
+            msg_placeholder.toast(result_msg, icon='❌')
+            continue
+        msg_with_data = result_msg.format(title)
+        msg_placeholder.toast(msg_with_data, icon='✅')
+        log_downloaded_files(content_type, title, file_path)
+        mime_type = 'audio/mp3' if is_mp3() else 'video/mp4'
+        insert_btn_to_save_media_locally(content_type.lower(), mime_type, title, file_path)
 
 
+def read_lines(s):
+    """s: any multiline string"""
+    return [url.strip() for url in s.split('\n') if url.strip() != '']
+
+
+if __name__ == '__main__':
+    st.set_page_config(
+        layout='centered', page_title='Youtube Video Downloader', page_icon='logo.png')
+
+    logo_col, heading_col = st.columns([1, 10])
+
+    with logo_col:
+        st.image('logo.png', width=80)
+    with heading_col:
+        st.title('Youtube Video Downloader')
+
+    st.divider()
+    user_input = st.text_area(
+        'Paste one or more YouTube URLs, one per line:', key='url')
+    st.divider()
+    dl_btn_cols = st.columns(2)
+    with dl_btn_cols[0]:
+        dl_as_vid = st.button('Download URLs as Video', help='Download the YouTube URLs as video', use_container_width=True)
+
+    with dl_btn_cols[1]:
+        dl_as_aud = st.button('Download URLs as Audio only', help='Download the YouTube URLs as audio', use_container_width=True)
+
+    if dl_as_vid:
+        download_all_urls_and_log(read_lines(user_input), file_extension='mp4')
+
+    if dl_as_aud:
+        download_all_urls_and_log(read_lines(user_input), file_extension='mp3')
